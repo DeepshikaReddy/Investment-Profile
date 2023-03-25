682c8f8e58828ae0566ec0043430817cb294ee3f
DeepshikaReddy <82325528+DeepshikaReddy@users.noreply.github.com>
2023-03-25 01:36:37 -0400

    added serach icons

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -15,15 +15,43 @@ const TopBar = ()=>{
     //Pass value from Parent to Child Component.
     const colorMode = useContext(ColorModeContext);
 
-
-    return 
     //Similar to <div> but can add css directly to it.
     //padding set to 2 ,justifyContent distributes child element evetually on main axis of flx container. 
-    (<Box display="flex" justifyContent="space-between" p={2}>
+    //InputBase is for input tags
+    return(<Box display="flex" justifyContent="space-between" p={2}>
         {/* SEARCH BAR */}
+        <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">   
+        <InputBase sx={{ml:2,flex:1}} placeholder="Search"/>
+        <IconButton type="button" sx={{p:1}}>
+        <SearchIcon />
+        </IconButton>
+        </Box>
+
+        {/* ICONS*/}
+        <box display="flex">
+        <IconButton>
+        <NotificationsOutlinedIcon/>
+        </IconButton>
+
+        <IconButton>
+        <SettingsOutlinedIcon/>
+        </IconButton>
+
+        <IconButton>
+        <PersonOutlinedIcon/>
+        </IconButton>
+
+        <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode ==="dark"? (
+            <DarkModeOutlinedIcon/>
+            ) :  <LightModeOutlinedIcon/>}
         
+        </IconButton>
+        </box>
+
     <IconButton sx={{display:"flex"}}></IconButton>
     </Box>)
+    
 }
 
 export default TopBar;
