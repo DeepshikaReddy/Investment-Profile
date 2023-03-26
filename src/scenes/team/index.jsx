8b70503fc968e54a5ec550311e3b8ff5e42cc65b
DeepshikaReddy <82325528+DeepshikaReddy@users.noreply.github.com>
2023-03-26 18:12:52 -0400

    Team screen css updated

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -37,23 +37,25 @@ const Team = () =>{
           field: "first_name",
           headerName: "First Name",
           flex: 1, // take equal space 
-          cellClassName: "name-column--cell", //each cell has class name value
+          cellClassName: "name-column--val", //each cell has class name value
         },
         {
             field: "last_name",
             headerName: "Last Name",
             flex: 1, // take equal space 
-            cellClassName: "name-column--cell", //each cell has class name value
+            cellClassName: "name-column--val", //each cell has class name value
           },
         {
           field: "phone",
-          headerName: "Phone NumberContact",
+          headerName: "Contact",
           flex: 1,
+          cellClassName: "name-column--val",
         },
         {
           field: "email",
           headerName: "Email Address",
           flex: 1,
+          cellClassName: "name-column--val",
         },
         {
             field: "avatar",
@@ -95,6 +97,8 @@ const Team = () =>{
 
     useEffect(()=>{
         const fetchData = async ()=>{
+
+            //Rest Api call.
            const {data} = await axios.get("https://reqres.in/api/users");
            console.log(data)
            const updatedData = data.data.map((row)=>{
@@ -113,8 +117,34 @@ const Team = () =>{
     return(
         <Box m ="20px">
             <Header title="TEAMS" subtitle="Managing Teams"></Header>
-            <Box m="50px 0 0 0" height="75vh">
+            <Box m="50px 0 0 0" height="75vh" sx={{
+          //Remove the borders 
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--val": {
+            color: colors.greenAccent[400],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[600],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}>
                 <DataGrid
+                checkboxSelection
                 rows = {rows}
                 columns = {columns}
                 />
